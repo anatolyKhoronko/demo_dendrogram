13d49e399ecfc06cbe9bfa573701f6f72b49fa6e
Anatolii <anatolii@bambu.life>
2019-10-08 15:26:00 +0300

    init commit

---
+++ dendrogram.js
@@ -0,0 +1,72 @@
+const width = 1200;
+const height = 1000;
+
+const cluster = d3.layout.cluster().size([height, width]);
+
+const svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(100,0)');
+const ys = [];
+
+const getXYfromJSONTree = node => {
+  ys.push(node.node_dist);
+  if (typeof node.children != 'undefined') {
+    for (let j in node.children) {
+      getXYfromJSONTree(node.children[j]);
+    }
+  }
+};
+
+let ymax = Number.MIN_VALUE;
+let ymin = Number.MAX_VALUE;
+
+getXYfromJSONTree(data); // data from ./data.js
+const nodes = cluster.nodes(data);
+const links = cluster.links(nodes);
+nodes.forEach((d, i) => {
+  if (typeof ys[i] != 'undefined') { d.y = ys[i]; }
+  if (d.y > ymax) ymax = d.y;
+  if (d.y < ymin) ymin = d.y;
+});
+
+const scale = d3.scale.linear().domain([ymin, ymax]).range([0, height]);
+const scaleInv = d3.scale.linear().domain([ymax, ymin]).range([0, height]);
+
+const elbow = (d, i) => {
+   if(d.source.name === 'Root1'){ // temporary solution
+     return null;
+  }
+  return 'M' + d.source.x + ',' + scale(d.source.y)
+    + 'H' + d.target.x + 'V' + scale(d.target.y) ;
+};
+
+svg.selectAll('.link')
+  .data(links)
+  .enter().append('path')
+  .attr('class', 'link')
+  .attr('d', elbow);
+
+const node = svg.selectAll('.node')
+  .data(nodes)
+  .enter().append('g')
+  .attr('class', 'node')
+  .attr('transform', d =>  'translate(' + d.x + ',' + scale(d.y) + ')');
+
+// node.append('circle')
+//   .attr('r', 0.5);
+
+const g = d3.select('svg').append('g')
+  .attr('transform', 'translate(100,40)');
+
+g.selectAll('.label')
+  .data(scale.ticks(5))
+  .enter().append('text')
+  .attr('class', 'label')
+  .text(String)
+  .attr('y', d => scaleInv(d))
+  .attr('x', -5)
+  .attr('text-anchor', 'middle');
+
+svg.attr('transform', `rotate(180, ${width / 2}, ${height / 2})`);
